409364207fb2f7e0ca34f9e0212d5769c61ce903
Maurício Maniglia <mauricio@MacBook-Pro-de-Mauricio.local>
2016-04-04 09:14:35 -0300

    Implemented ListDataManager class

--- Diligence/ListDataManager.swift
+++ Diligence/ListDataManager.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class ListDataManager: NSObject {
+    let session: NSURLSession = {
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        return NSURLSession(configuration: config)
+    }()
+    
+    func fetchDiligences(completion completion: ([DiligenceItem]) -> Void) {
+        
+        let url = ClientAPI.diligences()
+        let request = NSMutableURLRequest(URL: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-type")
+        request.setValue("Token <d8326786c9028d42e8f7d48fe799406f0e2e294e>", forHTTPHeaderField: "Authorization")
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) -> Void in
+            
+            let result = self.processDiligencesRequest(data: data, error: error)
+            let diligencesArray: [DiligenceItem]
+            
+            if case let .Success(diligences) = result {
+                diligencesArray = diligences
+            }
+            else {
+                diligencesArray = []
+            }
+            
+            completion(diligencesArray)
+        })
+        task.resume()
+    }
+    
+    func processDiligencesRequest(data data: NSData?, error: NSError?) -> DiligenceResult {
+        guard let jsonData = data else {
+            return .Failure(error!)
+        }
+        
+        return ClientAPI.diligencesFromJSONData(jsonData)
+    }
+}
