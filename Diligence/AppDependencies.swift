926b694ccae8b8c913a3c962e509ee9cf1670124
Maurício Maniglia <mauricio@MacBook-Pro-de-Mauricio.local>
2016-04-04 10:21:24 -0300

    Applied dependecies and change nav color

--- Diligence/AppDependencies.swift
+++ Diligence/AppDependencies.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import UIKit
+
+class AppDependencies {
+    var listWireframe = ListWireframe()
+    
+    init() {
+        configureDependencies()
+    }
+    
+    func installRootViewControllerIntoWindow(window: UIWindow) {
+        listWireframe.presentListInterfaceFromWindow(window)
+    }
+    
+    func configureDependencies() {
+        let rootWireframe = RootWireframe()
+        
+        let listPresenter = ListPresenter()
+        let listDataManager = ListDataManager()
+        let listInteractor = ListInteractor(dataManager: listDataManager)
+        
+        /*let addWireframe = AddWireframe()
+        let addInteractor = AddInteractor()
+        let addPresenter = AddPresenter()
+        let addDataManager = AddDataManager()*/
+        
+        listInteractor.output = listPresenter
+        
+        listPresenter.listInteractor = listInteractor
+        listPresenter.listWireframe = listWireframe
+        
+        //listWireframe.addWireframe = addWireframe
+        listWireframe.listPresenter = listPresenter
+        listWireframe.rootWireframe = rootWireframe                
+        
+        /*addInteractor.addDataManager = addDataManager
+        
+        addWireframe.addPresenter = addPresenter
+        
+        addPresenter.addWireframe = addWireframe
+        addPresenter.addModuleDelegate = listPresenter
+        addPresenter.addInteractor = addInteractor*/                
+    }
+}
